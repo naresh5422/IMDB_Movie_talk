f2db45c8613007d12a5ca955113d39de8ccc69fb
naresh5422 <naresh09022cm023@gmail.com>
2023-12-21 00:13:30 +0530

    change setup details

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='IMDB_Movie_Review',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Naresh',
+author_email='t.naresh5422@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
